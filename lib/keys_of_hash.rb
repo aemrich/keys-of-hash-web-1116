7f8c0c1ec1cdd74b85eab469354d3d4b74e781d6
aemrich <adam.emrich@gmail.com>
2016-11-23 18:32:30 -0500

    completed. accepting multiple arguments ftw.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    each do |key, value|
+      arguments.each do |x|
+        if value == x
+          array << key
+        end
+      end
+    end
+    array
   end
 end
